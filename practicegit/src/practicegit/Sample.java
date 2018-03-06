818c378cbf3e53e0f536c5a54ccbaaeec0f5e0e7
idoitforlove <21500671@handong.edu>
2018-03-07 00:12:57 +0900

    first commit

---
+++ practicegit/src/practicegit/Sample.java
@@ -0,0 +1,13 @@
+package practicegit;
+
+public class Sample {
+
+	public Sample() {
+		System.out.println("sample message");
+	}
+
+	public static void main(String[] args) {
+		new Sample();
+	}
+
+}
