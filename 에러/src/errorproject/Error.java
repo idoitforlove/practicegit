e48eb4bf85c62578d7e39366b8bed3e17b7a1cf2
idoitforlove <21500671@handong.edu>
2018-03-19 21:33:02 +0900

    error project

---
+++ 에러/src/errorproject/Error.java
@@ -0,0 +1,13 @@
+package errorproject;
+
+/* This program is for javaDoc error checking.
+ * javadoc: error - cannot read Input length = 1
+ * @author jcm
+ * */
+
+public class Error {
+	public static void main (String args[])
+	{
+		System.out.println("Hello");	
+	}
+}
